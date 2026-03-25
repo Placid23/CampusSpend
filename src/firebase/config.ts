f41eb251ff57e1a6d0c9a1ab8cabd6c72d05f068
Kingsley Enyinnaya <enyikings3778@gmail.com>
2026-03-25 18:25:01 +0000

    have you cnnected the app to the firebase backend // Import the function

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,9 @@
 export const firebaseConfig = {
-  "projectId": "studio-1667237920-b5675",
-  "appId": "1:956344002003:web:1d9ea5cddbec298d17bb26",
-  "apiKey": "AIzaSyDKtByHQG2EYm98eSeySStHo83fL5D1qB4",
-  "authDomain": "studio-1667237920-b5675.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "956344002003"
+  "apiKey": "AIzaSyDSyIP2UEuXk387laHvXkzcCO9lSyrCWUU",
+  "authDomain": "campusspend-733ab.firebaseapp.com",
+  "projectId": "campusspend-733ab",
+  "storageBucket": "campusspend-733ab.firebasestorage.app",
+  "messagingSenderId": "525745910718",
+  "appId": "1:525745910718:web:8f699c80f561cfd9acffef",
+  "measurementId": "G-KLEVQW11XM"
 };
